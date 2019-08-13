99f4acfd3197979de2b44bb9bdb78a98b894687e
clee1994 <clemensschaefer1994@gmail.com>
2019-08-12 20:36:35 -0400

    “test”

--- simple_nn.py
+++ simple_nn.py
@@ -13,24 +13,33 @@ from quantization import quantize
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+
 transform = transforms.Compose(
     [transforms.ToTensor()])
 
 
 # MNIST
-train_dataset = torchvision.datasets.MNIST('../data/MNIST', train=True, transform=transform, download=True)
+# train_dataset = torchvision.datasets.MNIST('../data/MNIST', train=True, transform=transform, download=True)
+# trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=512,
+#                                           shuffle=True, num_workers=2)
+# test_dataset = torchvision.datasets.MNIST('../data/MNIST', train=False, transform=transform, download=True)
+# testloader = torch.utils.data.DataLoader(test_dataset, batch_size=512,
+#                                           shuffle=True, num_workers=2)
+
+# CIFAR10
+train_dataset = torchvision.datasets.CIFAR10('../data/CIFAR10', train=True, transform=transform, download=True)
 trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=512,
                                           shuffle=True, num_workers=2)
-test_dataset = torchvision.datasets.MNIST('../data/MNIST', train=False, transform=transform, download=True)
+test_dataset = torchvision.datasets.CIFAR10('../data/CIFAR10', train=False, transform=transform, download=True)
 testloader = torch.utils.data.DataLoader(test_dataset, batch_size=512,
                                           shuffle=True, num_workers=2)
 
 
 # FMNIST
-# train_dataset = torchvision.datasets.FashionMNIST('../data/MNIST', train=True, transform=transform, download=True)
+# train_dataset = torchvision.datasets.FashionMNIST('../data/FMNIST', train=True, transform=transform, download=True)
 # trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=512,
 #                                           shuffle=True, num_workers=2)
-# test_dataset = torchvision.datasets.FashionMNIST('../data/MNIST', train=False, transform=transform, download=True)
+# test_dataset = torchvision.datasets.FashionMNIST('../data/FMNIST', train=False, transform=transform, download=True)
 # testloader = torch.utils.data.DataLoader(test_dataset, batch_size=512,
 #                                           shuffle=True, num_workers=2)
 
@@ -42,7 +51,7 @@ class Net(nn.Module):
 
         self.quant_nb = quant_nb
 
-        self.fc1 = nn.Linear(28*28, 800)
+        self.fc1 = nn.Linear(32*32, 800)
         torch.nn.init.orthogonal_(self.fc1.weight)
         self.fc2 = nn.Linear(800, 10)
         torch.nn.init.orthogonal_(self.fc2.weight)
@@ -51,9 +60,11 @@ class Net(nn.Module):
         x = x.view(-1, 28*28)
         with torch.no_grad():
             self.fc1.weight.data = quantize(self.fc1.weight.data, nb=self.quant_nb)
+            x = quantize(x, nb=self.quant_nb)
         x = torch.nn.functional.relu(self.fc1(x))
         with torch.no_grad():
             self.fc2.weight.data = quantize(self.fc2.weight.data, nb=self.quant_nb)
+            x = quantize(x, nb=self.quant_nb)
         x = torch.nn.functional.relu(self.fc2(x))
         return x
 
@@ -99,14 +110,17 @@ def train_nn(net, epochs):
             # print statistics
             running_loss = loss.item()
 
-            #with torch.no_grad():
-            #    net.fc1.weight.data = quantize(net.fc1.weight.data, nb=4)
-            #    net.fc2.weight.data = quantize(net.fc2.weight.data, nb=4)
         train_acc.append(get_accuracy(trainloader, net))
         test_acc.append(get_accuracy(testloader, net))
         print("Epoch %d, Train %.4f, Test %.4f" % (epoch, train_acc[-1], test_acc[-1]))
 
     return train_acc, test_acc
+
+net = Net(quant_nb = 8).to(device)
+train_acc, test_acc = train_nn(net, 500)
+
+
+
 
 record_train = []
 record_test = []
