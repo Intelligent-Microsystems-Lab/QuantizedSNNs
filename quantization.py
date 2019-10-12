af1073cc34e32ab3a19d03b5b4cf139620573977
clee1994 <clemensschaefer1994@gmail.com>
2019-10-12 14:17:44 -0400

    experiments setup

--- quantization.py
+++ quantization.py
@@ -1,82 +1,208 @@
-import numpy as np
+import math
 import torch
-
-def normalize_distribution(mu, var, min_r=-1,max_r=1):
-    # https://www.cs.princeton.edu/courses/archive/fall08/cos436/Duda/PR_Mahal/scaling.htm
-    new_mu = (max_r-min_r)*(mu-mu.min())/(mu.max()-mu.min())+min_r
-    new_v = abs(new_mu)/(abs(mu)/var)
-    return new_mu, new_v
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
 
 
-def clip_through(x, min, max):
-    '''Element-wise rounding to the closest integer with full gradient propagation.
-    A trick from [Sergey Ioffe](http://stackoverflow.com/a/36480182)
-    '''
-    clipped = torch.clamp(x,min,max) #clamp
-    return x + (clipped - x)
+# global quantization variables
+global_wb = 2
+global_ab = 8
+global_gb = 8
+global_eb = 8
+global_rb = 16
+
+global_beta = 1.5
+global_lr = 8
 
 
-def round_through(x):
-    '''Element-wise rounding to the closest integer with full gradient propagation.
-    A trick from [Sergey Ioffe](http://stackoverflow.com/a/36480182)
-    '''
-    rounded = torch.round(x)
-    rounded_through = x + (rounded - x)
-    return rounded_through
+# Quant Functions
+def step_d(bits): 
+    return 2.0 ** (bits - 1)
 
+def shift(x):
+    if x == 0:
+        return 1
+    return 2 ** torch.round(torch.log(x) / np.log(2))
 
-def quantize(weights, nb = 16, clip_through=False):
-
-    '''The weights' binarization function, 
-
-    # Reference:
-    - [QuantizedNet: Training Deep Neural Networks with Weights and Activations Constrained to +1 or -1, Courbariaux et al. 2016](http://arxiv.org/abs/1602.02830}
-
-    '''
-    non_sign_bits = nb-1
-    m = pow(2,non_sign_bits)
-    #W = tf.Print(W,[W],summarize=20)
-    if clip_through:
-        Wq = clip_through(round_through(weights*m),-m,m-1)/m
+def clip(x, bits):
+    if bits == 1:
+        delta = 0.
     else:
-        Wq = torch.clamp(round_through(weights*m),-m,m-1)/m
-    #Wq = tf.Print(Wq,[Wq],summarize=20)
-    return Wq
+        delta = 1./step_d(bits)
+    maxv = +1 - delta
+    minv = -1 + delta
+    return torch.clamp(x, float(minv), float(maxv))
 
-# def quantize(weights, mu, var):
-#   m = torch.distributions.normal.Normal(mu, var)
+def quant(x, bits):
+    if bits == 1: # BNN
+        return torch.sign(x)
+    else:
+        scale = step_d(bits)
+        return torch.round(x * scale ) / scale
 
-#   for i,layer_w in enumerate(weights):
-#       dim = layer_w.shape
-#       layer_w = layer_w.flatten()
-#       m_temp = m.sample([layer_w.shape[0]])
-#       temp_diff = torch.abs(m_temp - layer_w[:,None])
-#       _, ind_m = temp_diff.min(dim=1)
-#       weights[i] = torch.gather(input = m_temp, dim = 1, index = ind_m.view(-1,1)).reshape(dim)
-#       weights[i].requires_grad = True
+def quant_w(x, scale = 1):
+    with torch.no_grad():
+        y = quant(clip(x, global_wb) , global_wb)
+        diff = (y - x)
 
-#   return weights
+    if scale <= 1.8:
+        return x + diff
+    return (x + diff)/scale
 
+def quant_act(x):
+    save_x = x
+    x = clip(x, global_ab)
+    diff_map = (save_x == x)
+    with torch.no_grad():
+        y = quant(x, global_ab)
+        diff = y - x
+    return x + diff, diff_map
 
-#        (b-a)(x - min)
-# f(x) = --------------  + a
-#           max - min
+def quant_grad(x):
+    xmax = torch.max(torch.abs(x))
+    x = x / shift(xmax)
 
-# mu1 = torch.Tensor([.1022, .368, .822, 1.36, 1.95, 2.55, 2.92, 3.36])*10e-6
+    norm = quant(global_lr * x, global_rb)
 
-# diff1 = mu1[1:] - mu1[:-1]
-# diff2 = mu2[1:] - mu2[:-1]
+    norm_sign = torch.sign(norm)
+    norm_abs = torch.abs(norm)
+    norm_int = torch.floor(norm_abs)
+    norm_float = norm_abs - norm_int
+    rand_float = torch.FloatTensor(x.shape).uniform_(0,1).to(x.device)
+    norm = norm_sign.double() * ( norm_int.double() + 0.5 * (torch.sign(norm_float.double() - rand_float.double()) + 1) )
 
-# mu = torch.Tensor([.254, .589, .997, 1.3, 1.72, 2.24, 2.8, 3.36]).to(device)*10e-6
-# var = torch.Tensor([5.8, 4.92, 5.91, 5.91, 7.57, 10.9, 12.1, 12.5]).to(device)*10e-8
+    return norm / step_d(global_gb)
 
-# new_mu, new_v = normalize_distribution(mu, var)
+def quant_err(x):
+    alpha = shift(torch.max(torch.abs(x)))
+    return quant(clip(x / alpha, global_eb), global_eb)
 
-# test routine
-#weights = [torch.Tensor([-.8, .7, 5, -.002]), torch.Tensor([-.8, .7, 5, -.002])]
+def init_layer_weights(weights_layer, shape, factor=1):
+    fan_in = shape
 
-#mu = torch.Tensor([-1.1, -.61, -.29, .002, .40, .74, 1.2]) #3 bit precision
-#var = torch.Tensor([.01, .009, .008, .01, .009, .008, .01])
+    limit = torch.sqrt(torch.tensor([3*factor/fan_in]))
+    Wm = global_beta/step_d(torch.tensor([float(global_wb)]))
+    scale = 2 ** round(math.log(Wm / limit, 2.0))
+    scale = scale if scale > 1 else 1.0
+    limit = Wm if Wm > limit else limit
 
-#n_mu, n_var = normalize_distribution(mu, var)
-#w = quantize(weights, n_mu, n_var)
+    torch.nn.init.uniform_(weights_layer, a = -float(limit), b = float(limit))
+    return torch.tensor([float(scale)])
+
+# sum of square errors
+def SSE(y_true, y_pred):
+    return 0.5 * torch.sum((y_true.double() - y_pred.double())**2)
+
+def to_cat(inp_tensor, num_class, device):
+    out_tensor = torch.zeros([inp_tensor.shape[0], num_class], device=device)
+    out_tensor[torch.arange(inp_tensor.shape[0]).to(device), torch.tensor(inp_tensor, dtype = int, device=device)] = 1
+    return out_tensor
+
+# Inherit from Function
+class clee_LinearFunction(torch.autograd.Function):
+
+    # Note that both forward and backward are @staticmethods
+    @staticmethod
+    # bias is an optional argument
+    def forward(ctx, input, weight, scale, act, act_q, bias=None):
+        # prep and save
+        w_quant = quant_w(weight, scale)
+        input = input.float()
+        
+        # compute output
+        output = input.mm(w_quant.t())
+
+        relu_mask = torch.ones(output.shape).to(output.device)
+        clip_info = torch.ones(output.shape).to(output.device)
+
+        # add relu and quant optionally
+        if act:
+            output = F.relu(output)
+            relu_mask = (output != 0)
+        if act_q:
+            output, clip_info = quant_act(output)
+        if bias is not None:
+            output += bias.unsqueeze(0).expand_as(output)
+
+        ctx.save_for_backward(input, weight, w_quant, bias, relu_mask, clip_info)
+        return output
+
+    # This function has only a single output, so it gets only one gradient
+    @staticmethod
+    def backward(ctx, grad_output):
+        input, weight, w_quant, bias, relu_mask, clip_info = ctx.saved_tensors
+        grad_input = grad_weight = grad_bias = None
+        quant_error = quant_err(grad_output) * relu_mask.float() * clip_info.float()
+
+        if ctx.needs_input_grad[0]:
+            # propagate quantized error
+            grad_input = quant_error.mm(w_quant)
+        if ctx.needs_input_grad[1]:
+            grad_weight = quant_grad(quant_error.t().mm(input)).float()
+            
+        if bias is not None and ctx.needs_input_grad[2]:
+            grad_bias = grad_output.sum(0).squeeze(0)
+
+        return grad_input, grad_weight, grad_bias, None, None
+
+# Inherit from Function
+class clee_conv2d(torch.autograd.Function):
+    # Note that both forward and backward are @staticmethods
+    @staticmethod
+    # bias is an optional argument
+    def forward(ctx, input, weight, scale, act=False, act_q=False, pool=False, bias=None):
+        mpool1 = nn.MaxPool2d(2, stride=2, return_indices=True)
+
+        # prep and save
+        w_quant = quant_w(weight, scale)
+        input = input.float()
+        
+        # compute output
+        output = F.conv2d(input, w_quant, bias=None, stride=1, padding=0, dilation=1, groups=1)
+
+        relu_mask = torch.ones(output.shape).to(output.device)
+        clip_info = torch.ones(output.shape).to(output.device)
+        pool_indices = torch.ones(output.shape).to(output.device)
+        size_pool = torch.tensor([0])
+
+        # add pool, relu, quant optionally
+        if pool:
+            size_pool = output.shape
+            output, pool_indices = mpool1(output)
+        if act:
+            output = F.relu(output)
+            relu_mask = (output != 0)
+        if act_q:
+            output, clip_info = quant_act(output)
+        if bias is not None:
+            output += bias.unsqueeze(0).expand_as(output)
+
+        ctx.save_for_backward(input, weight, w_quant, bias, torch.tensor([pool]), relu_mask, clip_info, pool_indices, torch.tensor(size_pool))
+        return output
+
+    # This function has only a single output, so it gets only one gradient
+    @staticmethod
+    def backward(ctx, grad_output):
+        unpool1 = nn.MaxUnpool2d(2, stride=2, padding = 0)
+        
+        input, weight, w_quant, bias, pool, relu_mask, clip_info, pool_indices, size_pool = ctx.saved_tensors
+        grad_input = grad_weight = grad_bias = None
+
+        grad_output = grad_output * relu_mask.float() * clip_info.float()
+        if pool:
+            grad_output = unpool1(grad_output, pool_indices, output_size = torch.Size(size_pool))
+
+        quant_error = quant_err(grad_output) 
+
+        if ctx.needs_input_grad[0]:
+            # propagate quantized error
+            grad_input = torch.nn.grad.conv2d_input(input.shape, w_quant, quant_error)
+        if ctx.needs_input_grad[1]:
+            grad_weight = quant_grad(torch.nn.grad.conv2d_weight(input, weight.shape, quant_error)).float()
+
+        if bias is not None and ctx.needs_input_grad[2]:
+            grad_bias = grad_output.sum(0).squeeze(0)
+
+        return grad_input, grad_weight, grad_bias, None, None, None
--- wage_conv_quant_mnist.py
+++ wage_conv_quant_mnist.py
@@ -10,208 +10,17 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-torch.set_printoptions(precision=7)
+import quantization
+from quantization import clee_conv2d, clee_LinearFunction, quant_act, init_layer_weights, SSE, to_cat
 
-# global quantization variables
-global_wb = 2
-global_ab = 8
-global_gb = 8
-global_eb = 8
-global_rb = 16
+quantization.global_wb = 2
+quantization.global_ab = 8
+quantization.global_gb = 8
+quantization.global_eb = 8
+quantization.global_rb = 16
 
-global_beta = 1.5
-global_lr = 1
-
-# Quant Functions
-
-def step_d(bits): 
-    return 2.0 ** (bits - 1)
-
-def shift(x):
-    if x == 0:
-        return 1
-    return 2 ** torch.round(torch.log(x) / np.log(2))
-
-def clip(x, bits):
-    if bits == 1:
-        delta = 0.
-    else:
-        delta = 1./step_d(bits)
-    maxv = +1 - delta
-    minv = -1 + delta
-    return torch.clamp(x, float(minv), float(maxv))
-
-def quant(x, bits):
-    if bits == 1: # BNN
-        return torch.sign(x)
-    else:
-        scale = step_d(bits)
-        return torch.round(x * scale ) / scale
-
-def quant_w(x, scale = 1):
-    with torch.no_grad():
-        y = quant(clip(x, global_wb) , global_wb)
-        diff = (y - x)
-
-    if scale <= 1.8:
-        return x + diff
-    return (x + diff)/scale
-
-def quant_act(x):
-    save_x = x
-    x = clip(x, global_ab)
-    diff_map = (save_x == x)
-    with torch.no_grad():
-        y = quant(x, global_ab)
-        diff = y - x
-    return x + diff, diff_map
-
-def quant_grad(x):
-    xmax = torch.max(torch.abs(x))
-    x = x / shift(xmax)
-
-    norm = quant(global_lr * x, global_rb)
-
-    norm_sign = torch.sign(norm)
-    norm_abs = torch.abs(norm)
-    norm_int = torch.floor(norm_abs)
-    norm_float = norm_abs - norm_int
-    rand_float = torch.FloatTensor(x.shape).uniform_(0,1).to(x.device)
-    norm = norm_sign.double() * ( norm_int.double() + 0.5 * (torch.sign(norm_float.double() - rand_float.double()) + 1) )
-
-    return norm / step_d(global_gb)
-
-def quant_err(x):
-    alpha = shift(torch.max(torch.abs(x)))
-    return quant(clip(x / alpha, global_eb), global_eb)
-
-def init_layer_weights(weights_layer, shape, factor=1):
-    fan_in = shape
-
-    limit = torch.sqrt(torch.tensor([3*factor/fan_in]))
-    Wm = global_beta/step_d(torch.tensor([float(global_wb)]))
-    scale = 2 ** round(math.log(Wm / limit, 2.0))
-    scale = scale if scale > 1 else 1.0
-    limit = Wm if Wm > limit else limit
-
-    torch.nn.init.uniform_(weights_layer, a = -float(limit), b = float(limit))
-    return torch.tensor([float(scale)])
-
-# sum of square errors
-def SSE(y_true, y_pred):
-    return 0.5 * torch.sum((y_true.double() - y_pred.double())**2)
-
-def to_cat(inp_tensor, num_class, device):
-    out_tensor = torch.zeros([inp_tensor.shape[0], num_class], device=device)
-    out_tensor[torch.arange(inp_tensor.shape[0]).to(device), torch.tensor(inp_tensor, dtype = int, device=device)] = 1
-    return out_tensor
-
-# Inherit from Function
-class clee_LinearFunction(torch.autograd.Function):
-
-    # Note that both forward and backward are @staticmethods
-    @staticmethod
-    # bias is an optional argument
-    def forward(ctx, input, weight, scale, act, act_q, bias=None):
-        # prep and save
-        w_quant = quant_w(weight, scale)
-        input = input.float()
-        
-        # compute output
-        output = input.mm(w_quant.t())
-
-        relu_mask = torch.ones(output.shape).to(output.device)
-        clip_info = torch.ones(output.shape).to(output.device)
-
-        # add relu and quant optionally
-        if act:
-            output = F.relu(output)
-            relu_mask = (output != 0)
-        if act_q:
-            output, clip_info = quant_act(output)
-        if bias is not None:
-            output += bias.unsqueeze(0).expand_as(output)
-
-        ctx.save_for_backward(input, weight, w_quant, bias, relu_mask, clip_info)
-        return output
-
-    # This function has only a single output, so it gets only one gradient
-    @staticmethod
-    def backward(ctx, grad_output):
-        input, weight, w_quant, bias, relu_mask, clip_info = ctx.saved_tensors
-        grad_input = grad_weight = grad_bias = None
-        quant_error = quant_err(grad_output) * relu_mask.float() * clip_info.float()
-
-        if ctx.needs_input_grad[0]:
-            # propagate quantized error
-            grad_input = quant_error.mm(w_quant)
-        if ctx.needs_input_grad[1]:
-            grad_weight = quant_grad(quant_error.t().mm(input)).float()
-            
-        if bias is not None and ctx.needs_input_grad[2]:
-            grad_bias = grad_output.sum(0).squeeze(0)
-
-        return grad_input, grad_weight, grad_bias, None, None
-
-# Inherit from Function
-class clee_conv2d(torch.autograd.Function):
-    # Note that both forward and backward are @staticmethods
-    @staticmethod
-    # bias is an optional argument
-    def forward(ctx, input, weight, scale, act=False, act_q=False, pool=False, bias=None):
-        mpool1 = nn.MaxPool2d(2, stride=2, return_indices=True)
-
-        # prep and save
-        w_quant = quant_w(weight, scale)
-        input = input.float()
-        
-        # compute output
-        output = F.conv2d(input, w_quant, bias=None, stride=1, padding=0, dilation=1, groups=1)
-
-        relu_mask = torch.ones(output.shape).to(output.device)
-        clip_info = torch.ones(output.shape).to(output.device)
-        pool_indices = torch.ones(output.shape).to(output.device)
-        size_pool = torch.tensor([0])
-
-        # add pool, relu, quant optionally
-        if pool:
-            size_pool = output.shape
-            output, pool_indices = mpool1(output)
-        if act:
-            output = F.relu(output)
-            relu_mask = (output != 0)
-        if act_q:
-            output, clip_info = quant_act(output)
-        if bias is not None:
-            output += bias.unsqueeze(0).expand_as(output)
-
-        ctx.save_for_backward(input, weight, w_quant, bias, torch.tensor([pool]), relu_mask, clip_info, pool_indices, torch.tensor(size_pool))
-        return output
-
-    # This function has only a single output, so it gets only one gradient
-    @staticmethod
-    def backward(ctx, grad_output):
-        unpool1 = nn.MaxUnpool2d(2, stride=2, padding = 0)
-        
-        input, weight, w_quant, bias, pool, relu_mask, clip_info, pool_indices, size_pool = ctx.saved_tensors
-        grad_input = grad_weight = grad_bias = None
-
-        grad_output = grad_output * relu_mask.float() * clip_info.float()
-        if pool:
-            grad_output = unpool1(grad_output, pool_indices, output_size = torch.Size(size_pool))
-
-        quant_error = quant_err(grad_output) 
-
-        if ctx.needs_input_grad[0]:
-            # propagate quantized error
-            grad_input = torch.nn.grad.conv2d_input(input.shape, w_quant, quant_error)
-        if ctx.needs_input_grad[1]:
-            grad_weight = quant_grad(torch.nn.grad.conv2d_weight(input, weight.shape, quant_error)).float()
-
-        if bias is not None and ctx.needs_input_grad[2]:
-            grad_bias = grad_output.sum(0).squeeze(0)
-
-        return grad_input, grad_weight, grad_bias, None, None, None
+quantization.global_beta = 1.5
+quantization.global_lr = 1
 
 
 class Net(nn.Module):
@@ -338,41 +147,56 @@ test_loader = torch.utils.data.DataLoader(
     batch_size=64, shuffle=False, num_workers = 2, pin_memory = True)
 
 
-# setting up the model
-model = Net(device).to(device)
-optimizer = optim.SGD(model.parameters(), lr=1, momentum=0, dampening=0, weight_decay=0, nesterov=False)
 
-# train
-teacc, teloss, taacc, taloss = [], [], [], []  
-for epoch in range(100):
-    acc, lossv = train(model, device, train_loader, optimizer, epoch)
-    taacc.append(acc)
-    taloss.append(lossv)
-    acc, lossv = test(model, device, test_loader)
-    teacc.append(acc)
-    teloss.append(lossv)
+w_b_list = [2, 4, 6, 8]
+a_list = [8, 10, 12]
 
-# graph results
-plt.clf()
-plt.ylabel('Accuracy')
-plt.xlabel('Epochs')
-plt.plot(taacc , label="Training Accuracy", color="black")
-plt.plot(teacc , label="Test Accuracy", color="blue")
-plt.legend(loc = 'best')
-plt.title("Accuarcy Pytorch WAGE Acc MNIST")
-
-plt.tight_layout()
-plt.savefig("figures/torch_wage_acc_mnist.png")
+for i in w_b_list:
+    for j in in a_list
+        quantization.global_wb = i
+        quantization.global_ab = j
+        quantization.global_gb = 8
+        quantization.global_eb = 8
+        quantization.global_rb = 16
 
 
-plt.clf()
-plt.ylabel('Loss')
-plt.xlabel('Epochs')
-plt.plot(taloss , label="Training Loss", color="black")
-plt.plot(teloss , label="Test Loss", color="blue")
-plt.legend(loc = 'best')
-plt.title("Loss Pytorch WAGE Loss MNIST")
+        # setting up the model
+        model = Net(device).to(device)
+        optimizer = optim.SGD(model.parameters(), lr=1, momentum=0, dampening=0, weight_decay=0, nesterov=False)
 
-plt.tight_layout()
-plt.savefig("figures/torch_wage_loss_mnist.png")
+        # train
+        teacc, teloss, taacc, taloss = [], [], [], []  
+        for epoch in range(3):
+            acc, lossv = train(model, device, train_loader, optimizer, epoch)
+            taacc.append(acc)
+            taloss.append(lossv)
+            acc, lossv = test(model, device, test_loader)
+            teacc.append(acc)
+            teloss.append(lossv)
+
+        # graph results
+        bit_string = str(quantization.global_wb) + str(quantization.global_ab) + str(quantization.global_gb) + str(quantization.global_eb)
+
+        plt.clf()
+        plt.ylabel('Accuracy')
+        plt.xlabel('Epochs')
+        plt.plot(taacc , label="Training Accuracy", color="black")
+        plt.plot(teacc , label="Test Accuracy", color="blue")
+        plt.legend(loc = 'best')
+        plt.title("Accuarcy Pytorch WAGE Acc MNIST ("+bit_string+")")
+
+        plt.tight_layout()
+        plt.savefig("figures/torch_wage_acc_mnist_"+bit_string+".png")
+
+
+        plt.clf()
+        plt.ylabel('Loss')
+        plt.xlabel('Epochs')
+        plt.plot(taloss , label="Training Loss", color="black")
+        plt.plot(teloss , label="Test Loss", color="blue")
+        plt.legend(loc = 'best')
+        plt.title("Loss Pytorch WAGE Loss MNIST ("+bit_string+")")
+
+        plt.tight_layout()
+        plt.savefig("figures/torch_wage_loss_mnist_"+bit_string+".png")
 
