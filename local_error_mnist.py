5690fe179c45872035f946c8ec3040eabc200d68
Clemens JS Schafer <clemens@clemenss-mbp.dhcp.nd.edu>
2020-02-12 10:59:50 -0500

    4wb

--- local_error_mnist.py
+++ local_error_mnist.py
@@ -383,7 +383,7 @@ y_train = y_train[index_list_train]
 y_test = y_test[index_list_test]
 
 quantization.global_beta = 1.5
-quantization.global_wb = 2
+quantization.global_wb = 4
 
 ms = 1e-3
 delta_t = 1*ms
