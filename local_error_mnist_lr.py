afaa9c3249791ca025313485c53269fa30140275
Clemens JS Schafer <clemens@clemenss-mbp.dhcp.nd.edu>
2020-02-13 10:14:03 -0500

    test

--- local_error_mnist_lr.py
+++ local_error_mnist_lr.py
@@ -375,8 +375,8 @@ num_classes = 10
 index_list_train = []
 index_list_test = []
 for i in range(10):
-    index_list_train.append((y_train == i).nonzero()[:train_samples/num_classes])
-    index_list_test.append((y_test == i).nonzero()[:test_samples/num_classes])
+    index_list_train.append((y_train == i).nonzero()[:int(train_samples/num_classes)])
+    index_list_test.append((y_test == i).nonzero()[:int(test_samples/num_classes)])
 index_list_train = torch.cat(index_list_train).reshape([train_samples])
 index_list_test = torch.cat(index_list_test).reshape([test_samples])
 
@@ -394,7 +394,7 @@ delta_t = 1*ms
 T = 500*ms
 T_test = 1000*ms
 burnin = 50*ms
-batch_size = 256
+batch_size = 64
 output_neurons = 10
 
 tau_mem = torch.Tensor([5*ms, 35*ms]).to(device)
@@ -425,106 +425,103 @@ opt1 = torch.optim.Adam(layer1.parameters(), lr=1e-5, betas=[0., .95])
 opt2 = torch.optim.Adam(layer2.parameters(), lr=1e-5, betas=[0., .95])
 opt3 = torch.optim.Adam(layer3.parameters(), lr=1e-5, betas=[0., .95])
 opt4 = torch.optim.Adam(layer4.parameters(), lr=1e-5, betas=[0., .95])
-scheduler1 = torch.optim.lr_scheduler.StepLR(opt1, step_size=20, gamma=0.5)
-scheduler2 = torch.optim.lr_scheduler.StepLR(opt2, step_size=20, gamma=0.5)
-scheduler3 = torch.optim.lr_scheduler.StepLR(opt3, step_size=20, gamma=0.5)
-scheduler4 = torch.optim.lr_scheduler.StepLR(opt4, step_size=20, gamma=0.5)
-
-print("Weight Quantization: {0}".format(quantization.global_wb))
-
-for e in range(60):
-    correct = 0
-    total = 0
-    tcorrect = 0
-    ttotal = 0
-    loss_hist = []
-    start_time = time.time()
-
-    for x_local, y_local in sparse_data_generator(x_train, y_train, batch_size = batch_size, nb_steps = T / ms, samples = train_samples, max_hertz = 50, shuffle = True, device = device):
-        class_rec = torch.zeros([x_local.shape[0], output_neurons]).to(device)
-
-        layer1.state_init(x_local.shape[0])
-        layer2.state_init(x_local.shape[0])
-        layer3.state_init(x_local.shape[0])
-        layer4.state_init(x_local.shape[0])
-
-        # burnin
-        for t in range(int(burnin/ms)):
-            out_spikes1 = layer1.forward(x_local[:,:,:,:,t])
-            out_spikes2 = layer2.forward(out_spikes1)
-            out_spikes3 = layer3.forward(out_spikes2)
-            out_spikes3 = out_spikes3.reshape([x_local.shape[0], np.prod(layer3.out_shape)])
-            out_spikes4 = layer4.forward(out_spikes3)
 
 
-        # training
-        for t in range(int(burnin/ms), int(T/ms)):
-            out_spikes1 = layer1.forward(x_local[:,:,:,:,t])
-            rreadout1 = random_readout1(dropout_learning(smoothstep(layer1.U.reshape([x_local.shape[0], np.prod(layer1.out_shape)]))))
-            y_log_p1 = log_softmax_fn(rreadout1)
-            loss_t1 = nll_loss(y_log_p1, y_local) + lambda1 * F.relu(layer1.U+.01).mean() + lambda2 * F.relu(thr-layer1.U).mean()
-            loss_t1.backward()
-            opt1.step()
-            opt1.zero_grad()
+# lr opt
+init_value = 1e-15
+final_value= 10
+beta = 0.98
 
-            out_spikes2 = layer2.forward(out_spikes1)
-            rreadout2 = random_readout2(dropout_learning(smoothstep(layer2.U.reshape([x_local.shape[0], np.prod(layer2.out_shape)]))))
-            y_log_p2 = log_softmax_fn(rreadout2)
-            loss_t2 = nll_loss(y_log_p2, y_local) + lambda1 * F.relu(layer2.U+.01).mean() + lambda2 * F.relu(thr-layer2.U).mean()
-            loss_t2.backward()
-            opt2.step()
-            opt2.zero_grad()
+num = np.ceil(train_samples/batch_size)-1
+mult = (final_value / init_value) ** (1/num)
+lr = init_value
 
-            out_spikes3 = layer3.forward(out_spikes2)
-            rreadout3 = random_readout3(dropout_learning(smoothstep(layer3.U.reshape([x_local.shape[0], np.prod(layer3.out_shape)]))))
-            y_log_p3 = log_softmax_fn(rreadout3)
-            loss_t3 = nll_loss(y_log_p3, y_local) + lambda1 * F.relu(layer3.U+.01).mean() + lambda2 * F.relu(thr-layer3.U).mean()
-            loss_t3.backward()
-            opt3.step()
-            opt3.zero_grad()
+opt1.param_groups[0]['lr'] = lr
+opt2.param_groups[0]['lr'] = lr
+opt3.param_groups[0]['lr'] = lr
+opt4.param_groups[0]['lr'] = lr
 
-            out_spikes3 = out_spikes3.reshape([x_local.shape[0], np.prod(layer3.out_shape)])
-            out_spikes4 = layer4.forward(out_spikes3)
-            y_log_p4 = log_softmax_fn(smoothstep(layer4.U))
-            loss_t4 = nll_loss(y_log_p4, y_local) + lambda1 * F.relu(layer4.U+.01).mean() + lambda2 * F.relu(.1-layer4.U).mean()
-            loss_t4.backward()
-            opt4.step()
-            opt4.zero_grad()
+avg_loss = 0.
+best_loss = 0.
+batch_num = 0
+losses = []
+log_lrs = []
 
-            loss_hist.append(loss_t4.item())
-            class_rec += out_spikes4
-
-        correct += (torch.max(class_rec, dim = 1).indices == y_local).sum() 
-        total += len(y_local)
-    train_time = time.time()
+print("Optimizing LR for Weight Quantization: {0}".format(quantization.global_wb))
 
 
-    # compute test accuracy
-    for x_local, y_local in sparse_data_generator(x_test, y_test, batch_size = batch_size, nb_steps = T_test/ms, samples = test_samples, max_hertz = 50, shuffle = True, device = device):
-        class_rec = torch.zeros([x_local.shape[0], output_neurons]).to(device)
-        layer1.state_init(x_local.shape[0])
-        layer2.state_init(x_local.shape[0])
-        layer3.state_init(x_local.shape[0])
-        layer4.state_init(x_local.shape[0])
+for x_local, y_local in sparse_data_generator(x_train, y_train, batch_size = batch_size, nb_steps = T / ms, samples = train_samples, max_hertz = 50, shuffle = True, device = device):
+    batch_num += 1
 
-        for t in range(int(T_test/ms)):
-            out_spikes1 = layer1.forward(x_local[:,:,:,:,t])
-            out_spikes2 = layer2.forward(out_spikes1)
-            out_spikes3 = layer3.forward(out_spikes2)
-            out_spikes3 = out_spikes3.reshape([x_local.shape[0], np.prod(layer3.out_shape)])
-            out_spikes4 = layer4.forward(out_spikes3)
-            class_rec += out_spikes4
-        tcorrect += (torch.max(class_rec, dim = 1).indices == y_local).sum() 
-        ttotal += len(y_local)
-    inf_time = time.time()
+    layer1.state_init(x_local.shape[0])
+    layer2.state_init(x_local.shape[0])
+    layer3.state_init(x_local.shape[0])
+    layer4.state_init(x_local.shape[0])
 
-    scheduler1.step()
-    scheduler2.step()
-    scheduler3.step()
-    scheduler4.step()
+    # burnin
+    for t in range(int(burnin/ms)):
+        out_spikes1 = layer1.forward(x_local[:,:,:,:,t])
+        out_spikes2 = layer2.forward(out_spikes1)
+        out_spikes3 = layer3.forward(out_spikes2)
+        out_spikes3 = out_spikes3.reshape([x_local.shape[0], np.prod(layer3.out_shape)])
+        out_spikes4 = layer4.forward(out_spikes3)
 
-    print("Epoch {0} | Loss: {1:.4f} Train Acc: {2:.4f} Test Acc: {3:.4f} Train Time: {4:.4f}s Inference Time: {5:.4f}s".format(e+1, np.mean(loss_hist), correct.item()/total, tcorrect.item()/ttotal, train_time-start_time, inf_time - train_time)) 
+    # training
+    loss_t_sum = 0
+    for t in range(int(burnin/ms), int(T/ms)):
+        out_spikes1 = layer1.forward(x_local[:,:,:,:,t])
+        rreadout1 = random_readout1(dropout_learning(smoothstep(layer1.U.reshape([x_local.shape[0], np.prod(layer1.out_shape)]))))
+        y_log_p1 = log_softmax_fn(rreadout1)
+        loss_t1 = nll_loss(y_log_p1, y_local) + lambda1 * F.relu(layer1.U+.01).mean() + lambda2 * F.relu(thr-layer1.U).mean()
+        loss_t1.backward()
+        opt1.step()
+        opt1.zero_grad()
 
+        out_spikes2 = layer2.forward(out_spikes1)
+        rreadout2 = random_readout2(dropout_learning(smoothstep(layer2.U.reshape([x_local.shape[0], np.prod(layer2.out_shape)]))))
+        y_log_p2 = log_softmax_fn(rreadout2)
+        loss_t2 = nll_loss(y_log_p2, y_local) + lambda1 * F.relu(layer2.U+.01).mean() + lambda2 * F.relu(thr-layer2.U).mean()
+        loss_t2.backward()
+        opt2.step()
+        opt2.zero_grad()
 
+        out_spikes3 = layer3.forward(out_spikes2)
+        rreadout3 = random_readout3(dropout_learning(smoothstep(layer3.U.reshape([x_local.shape[0], np.prod(layer3.out_shape)]))))
+        y_log_p3 = log_softmax_fn(rreadout3)
+        loss_t3 = nll_loss(y_log_p3, y_local) + lambda1 * F.relu(layer3.U+.01).mean() + lambda2 * F.relu(thr-layer3.U).mean()
+        loss_t3.backward()
+        opt3.step()
+        opt3.zero_grad()
 
+        out_spikes3 = out_spikes3.reshape([x_local.shape[0], np.prod(layer3.out_shape)])
+        out_spikes4 = layer4.forward(out_spikes3)
+        y_log_p4 = log_softmax_fn(smoothstep(layer4.U))
+        loss_t4 = nll_loss(y_log_p4, y_local) + lambda1 * F.relu(layer4.U+.01).mean() + lambda2 * F.relu(.1-layer4.U).mean()
+        loss_t4.backward()
+        opt4.step()
+        opt4.zero_grad()
 
+        loss_t_sum += loss_t4.item()
+
+    #Compute the smoothed loss
+    avg_loss = beta * avg_loss + (1-beta) * loss_t_sum
+    smoothed_loss = avg_loss / (1 - beta**batch_num)
+    #Stop if the loss is exploding
+    if batch_num > 1 and smoothed_loss > 4 * best_loss:
+        break
+    #Record the best loss
+    if smoothed_loss < best_loss or batch_num==1:
+        best_loss = smoothed_loss
+    #Store the values
+    losses.append(smoothed_loss)
+    log_lrs.append(math.log10(lr))
+
+    lr *= mult
+    opt1.param_groups[0]['lr'] = lr
+    opt2.param_groups[0]['lr'] = lr
+    opt3.param_groups[0]['lr'] = lr
+    opt4.param_groups[0]['lr'] = lr
+
+plt.clf()
+plt.plot(log_lrs,losses)
+plt.savefig('figures/conv_snn_mnist_lr')
