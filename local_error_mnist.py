d00f7661a892f98adf8784d5d00641dee33d4426
Clemens JS Schafer <clemens@50-204-251-125-static.hfc.comcastbusiness.net>
2020-02-13 23:09:57 -0500

    “test”

--- local_error_mnist.py
+++ local_error_mnist.py
@@ -347,10 +347,10 @@ y_train = y_train[index_list_train]
 y_test = y_test[index_list_test]
 
 quantization.global_beta = 1.5
-quantization.global_wb = 4
-quantization.global_ub = 4
-quantization.global_qb = 4
-quantization.global_pb = 4
+quantization.global_wb = 5
+quantization.global_ub = 5
+quantization.global_qb = 5
+quantization.global_pb = 5
 
 ms = 1e-3
 delta_t = 1*ms
