b4f514ae85273b5fc6ee6b29ba446d585e196f39
Clemens Schaefer <cschaef6@crcfe02.crc.nd.edu>
2020-02-18 11:30:59 -0500

    “test”

---
+++ experiments/test.py_02-18-20_11.30.10/test.py
@@ -0,0 +1,12 @@
+import pickle
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-dir", "--dir", type = str, help = "activation bits")
+args = vars(ap.parse_args())
+
+
+print("hello")
+
+with open(args['dir'] + '/test_output.pkl', 'wb') as f:
+    pickle.dump({"foo":"bar"}, f)
