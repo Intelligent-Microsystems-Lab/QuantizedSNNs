1959ba2e8ff7cd3d143d4b6fa7abd3367dad6118
Clemens JS Schafer <clemens@clemenss-mbp.dhcp.nd.edu>
2020-03-05 16:57:29 -0500

    test

--- search_local_error_dvs.py
+++ search_local_error_dvs.py
@@ -397,8 +397,8 @@ def objective(args):
 
 
 space = {
-    'mem_tau' : hp.uniform('mem_tau', 1, 130), 
-    'syn_tau' : 7.5,#hp.uniform('syn_tau', 1, 50), 
+    'mem_tau' : 20,#hp.uniform('mem_tau', 1, 130), 
+    'syn_tau' : hp.uniform('syn_tau', 1, 130), 
     'l1' :      .2,#hp.uniform('l1', 0, 1.5),
     'l2' :      .1,#hp.uniform('l2', 0, 1.5)
 }
