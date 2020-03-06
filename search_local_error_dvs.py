fdfbd20194ede9f82c2eb64b24d2e005096b5b19
Clemens JS Schafer <clemens@clemenss-mbp.dhcp.nd.edu>
2020-03-05 23:13:33 -0500

    test

--- search_local_error_dvs.py
+++ search_local_error_dvs.py
@@ -401,7 +401,7 @@ space = {
     'syn_tau' : 110,#hp.uniform('syn_tau', 1, 130), 
     'l1' :      hp.uniform('l1', .5, 1.5),#1.3,#
     'l2' :      hp.uniform('l2', .1, 1.5),#0.15,#
-    'var_perc' : hp.uniform('l2', 0, .9)
+    'var_perc' : hp.uniform('var_perc', 0, .9)
 }
 
 best = fmin(objective, space, algo=tpe.suggest, max_evals=75)
