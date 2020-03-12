eab1a081fce19edf6622b6459776734241731be5
Clemens JS Schafer <clemens@clemenss-mbp.dhcp.nd.edu>
2020-03-12 13:56:12 -0400

    test

--- local_error_dvs.py
+++ local_error_dvs.py
@@ -59,14 +59,14 @@ T_test = 1800*ms
 
 
 # set quant level
-quantization.global_wb = 32
-quantization.global_ub = 32
-quantization.global_qb = 32
-quantization.global_pb = 32
-quantization.global_gb = 32
-quantization.global_eb = 32
-quantization.global_rb = 32
-quantization.global_lr = 200#8
+quantization.global_wb = 16
+quantization.global_ub = 16
+quantization.global_qb = 16
+quantization.global_pb = 16
+quantization.global_gb = 16
+quantization.global_eb = 16
+quantization.global_rb = 16
+quantization.global_lr = 1#8
 quantization.global_sb = 1
 quantization.global_beta = 1.5#quantization.step_d(quantization.global_wb)-.5 #1.5 #
 
@@ -115,7 +115,7 @@ opt = torch.optim.SGD(all_parameters, lr=1)
 
 diff_layers_acc = {'train1': [], 'test1': [],'train2': [], 'test2': [],'train3': [], 'test3': []}
 
-print("WPQUEG Quantization: {0}{1}{2}{3}{4}{5}{6} l1 {7:.3f} l2 {8:.3f} Inp {9} LR {10}".format(quantization.global_wb, quantization.global_pb, quantization.global_qb, quantization.global_ub, quantization.global_eb, quantization.global_gb, quantization.global_sb, l1, l2, input_mode, quantization.global_lr))
+print("WPQUEG Quantization: {0}{1}{2}{3}{4}{5}{6} l1 {7:.3f} l2 {8:.3f} Inp {9} LR {10} Drop {11}".format(quantization.global_wb, quantization.global_pb, quantization.global_qb, quantization.global_ub, quantization.global_eb, quantization.global_gb, quantization.global_sb, l1, l2, input_mode, quantization.global_lr, dropout_p))
 
 
 for e in range(75):
