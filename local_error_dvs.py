836721d10a8c7e8ce5e740780942b07297d7a2ee
Clemens JS Schafer <clemens@clemenss-mbp.dhcp.nd.edu>
2020-03-11 22:56:33 -0400

    test

--- local_error_dvs.py
+++ local_error_dvs.py
@@ -59,12 +59,12 @@ T_test = 1800*ms
 
 
 # set quant level
-quantization.global_wb = 32
-quantization.global_ub = 32
-quantization.global_qb = 32
-quantization.global_pb = 32
-quantization.global_gb = 32
-quantization.global_eb = 32
+quantization.global_wb = 16
+quantization.global_ub = 16
+quantization.global_qb = 16
+quantization.global_pb = 16
+quantization.global_gb = 16
+quantization.global_eb = 16
 quantization.global_rb = 16
 quantization.global_lr = 1#8
 quantization.global_sb = 1
