0cdbe5140e69587e43419fe0c4bed8feaaeea50f
Clemens JS Schafer <clemens@clemenss-mbp.dhcp.nd.edu>
2020-03-19 13:34:06 -0400

    test

--- localQ.py
+++ localQ.py
@@ -294,6 +294,7 @@ class QSConv2dFunctional(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, grad_output):
+        import pdb; pdb.set_trace()
         input, w_quant, bias_quant, pool_indices = ctx.saved_tensors
         grad_input = grad_weight = grad_bias = None 
         unmpool = nn.MaxUnpool2d(ctx.pooling, stride = ctx.pooling, padding = (ctx.pooling-1)//2)
