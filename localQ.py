2f0a0043fd33cde1b1b061da0c8946449117cba6
Clemens JS Schafer <clemens@clemenss-mbp.dhcp.nd.edu>
2020-03-23 20:53:34 -0400

    test

--- localQ.py
+++ localQ.py
@@ -28,13 +28,12 @@ def create_graph(plot_file_name, diff_layers_acc):
     ax1.plot(t, diff_layers_acc['test2'], 'b-', label = 'Test 2')
     ax1.plot(t, diff_layers_acc['test3'], 'r-', label = 'Test 3')
     ax1.plot([], [], 'k-', label = 'Loss')
-    ax1.legend(bbox_to_anchor=(1.04,1), loc="upper left")
+    ax1.legend(bbox_to_anchor=(1.08,1), loc="upper left")
 
     ax2 = ax1.twinx()
     ax2.set_ylabel('Loss')
     ax2.plot(t, diff_layers_acc['loss'], 'k-', label = 'Loss')
 
-    plt.legend(loc='best')
     fig.tight_layout()
     plt.savefig(plot_file_name)
 
