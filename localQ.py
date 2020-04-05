54df8020852dfb6ef36e51c92f50df910055704f
Clemens JS Schafer <clemens@50-204-251-125-static.hfc.comcastbusiness.net>
2020-04-05 10:20:25 -0400

    test

--- localQ.py
+++ localQ.py
@@ -514,7 +514,7 @@ class LIFConv2dLayer(nn.Module):
             # unsure yet... first PQ
             self.R, _ = quantization.quant01(self.R, quantization.global_rfb)
 
-        self.P, self.R, self.Q = self.alpha * self.P + self.tau_mem * self.Q, self.gamma * self.R, self.beta * self.Q + self.tau_syn * input_t
+        #self.P, self.R, self.Q = self.alpha * self.P + self.tau_mem * self.Q, self.gamma * self.R, self.beta * self.Q + self.tau_syn * input_t
 
         self.P, self.R, self.Q = self.alpha * self.P + self.inp_mult_p * self.Q, self.gamma * self.R, self.beta * self.Q + self.inp_mult_q * input_t
 
