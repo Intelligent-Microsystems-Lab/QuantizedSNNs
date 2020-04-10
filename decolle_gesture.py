c5734838ac357b19ed3f14b3cfe0959f631f6f5f
Clemens JS Schafer <clemens@Clemenss-MacBook-Pro.local>
2020-04-10 00:13:28 -0400

    test

--- decolle_gesture.py
+++ decolle_gesture.py
@@ -68,8 +68,8 @@ y_test = np.array(data[1], dtype = int) - 1
 quantization.global_ab  = 8
 quantization.global_sig = 8
 quantization.global_sb  = 1
-quantization.global_gb  = 8
-quantization.global_eb  = 8
+quantization.global_gb  = None
+quantization.global_eb  = None
 
 quantization.global_wb  = 4
 quantization.global_ub  = 4
