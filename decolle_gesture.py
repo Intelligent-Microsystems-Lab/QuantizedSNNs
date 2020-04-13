4d5d31af0a883b80686324c8eda84d1e65374bec
Clemens JS Schafer <clemens@Clemenss-MacBook-Pro.local>
2020-04-12 22:20:00 -0400

    “test”

--- decolle_gesture.py
+++ decolle_gesture.py
@@ -68,7 +68,7 @@ y_test = np.array(data[1], dtype = int) - 1
 # base case 8 bits -> every variable gets its sweep (2,4,6,8)
 quantization.global_wb  = 8 # what 2bits/2bit
 quantization.global_ub  = 8 # 8 bits
-quantization.global_qb  = 4 
+quantization.global_qb  = 6 
 quantization.global_pb  = 8
 quantization.global_rfb = 8
 
