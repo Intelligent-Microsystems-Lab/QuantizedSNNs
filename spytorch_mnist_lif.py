1f7de4536bfc71b791b58c1b0d7102c3d5ad8274
Clemens JS Schafer <clemens@Clemenss-MacBook-Pro.local>
2020-04-13 12:42:21 -0400

    test

--- spytorch_mnist_lif.py
+++ spytorch_mnist_lif.py
@@ -45,7 +45,7 @@ nb_hidden  = args['hi']
 mult_eq = args['me']
 mult_thresh_sat = args['th']
 
-thres_variance = .26
+thres_variance = .28
 
 nb_steps  =  80 # 100 previously, some good results with 150
 
