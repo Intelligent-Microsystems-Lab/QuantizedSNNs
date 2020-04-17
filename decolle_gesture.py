bd832b3f8443cd5ec7ed1734e933d52e6da0e7eb
Clemens JS Schafer <clemens@Clemenss-MacBook-Pro.local>
2020-04-17 14:37:31 -0400

    hello

--- decolle_gesture.py
+++ decolle_gesture.py
@@ -102,8 +102,8 @@ quantization.global_pb  = 10
 quantization.global_rfb = 4
 
 quantization.global_sb  = 4
-quantization.global_gb  = 14
-quantization.global_eb  = 8
+quantization.global_gb  = 12
+quantization.global_eb  = 6
 
 quantization.global_ub  = 8
 quantization.global_ab  = 8
