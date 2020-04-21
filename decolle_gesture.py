323f12753dd7129699d577840555c6d5336e9371
Clemens JS Schafer <clemens@Clemenss-MacBook-Pro.local>
2020-04-20 21:32:41 -0400

    hello

--- decolle_gesture.py
+++ decolle_gesture.py
@@ -93,15 +93,15 @@ burnin = 50*ms
 x_size = 32
 y_size = 32
 
-change_diff = 2
+change_diff = -1
 
 # set quant level
 quantization.global_wb  = 8
-quantization.global_qb  = 10 + change_diff
-quantization.global_pb  = 12 + change_diff
+quantization.global_qb  = 10 
+quantization.global_pb  = 12 
 quantization.global_rfb = 2
 
-quantization.global_sb  = 6
+quantization.global_sb  = 6 + change_diff
 quantization.global_gb  = 10
 quantization.global_eb  = 6
 
