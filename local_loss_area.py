5bae8289b2a0d7daf92454bc559a897204e696a4
Clemens JS Schafer <clemens@50-204-251-125-static.hfc.comcastbusiness.net>
2020-05-08 17:41:48 -0400

    exp

--- local_loss_area.py
+++ local_loss_area.py
@@ -24,9 +24,9 @@ import line_profiler
 # BaseLong.pkl, PQLong.pkl, EGLong.pkl, NoneLong.pkl
 # PQShort.pkl, EGShort.pkl, NoneShort.pkl,
 
-read_file = 'results/BaseLong.pkl'
-surf_file = 'BaseLong.h5'
-log_file = open('logs/BaseLong.log', 'w') 
+read_file = 'results/PQLong.pkl'
+surf_file = 'PQLong.h5'
+log_file = open('logs/PQLong.log', 'w') 
 
 #torch.autograd.set_detect_anomaly(True)
 
