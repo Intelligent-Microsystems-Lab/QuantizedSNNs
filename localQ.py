5c7f6cca3771a0b95ed129d33d14fbf8115817ba
Clemens JS Schafer <clemens@50-204-251-125-static.hfc.comcastbusiness.net>
2020-05-22 00:34:43 -0400

    hello

--- localQ.py
+++ localQ.py
@@ -226,15 +226,15 @@ def sparse_data_generator_DVSGesture(X, y, batch_size, nb_steps, shuffle, device
 
         
         #change
-        import pdb; pdb.set_trace()
 
         change_mask = torch.bernoulli((shift_prob) * torch.ones(all_events.shape[0])).bool()
         forward_mask = change_mask * torch.bernoulli((.5) * torch.ones(all_events.shape[0])).bool()
         backward_mask = (change_mask != forward_mask)
+        import pdb; pdb.set_trace()
         all_events[forward_mask, 1] = all_events[forward_mask, 1] + 1
-        all_events[backward_mask, 1] = all_events[backward_mask, 1] + 1
-        all_events[all_events[:,1] == -1] = 0
-        all_events[all_events[:,1] == 500] = 499
+        all_events[backward_mask, 1] = all_events[backward_mask, 1] - 1
+        all_events[all_events[:,1] == -1][:,1] = 0
+        all_events[all_events[:,1] == 500][:1] = 499
 
 
         all_events = all_events[:,[0,4,2,3,1]]
