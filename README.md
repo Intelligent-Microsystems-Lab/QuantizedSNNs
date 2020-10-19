3a1461cf5e0601d24a1f0de5a95ed754dcfceeb6
Clemens JS Schaefer <15638937+clee1994@users.noreply.github.com>
2020-10-19 14:19:11 -0400

    cleanup

--- README.md
+++ README.md
@@ -4,12 +4,37 @@ This repository contains the models and training scripts used in the papers: ["Q
 
 ## Requiremtns
 
-- Python => 3
+- Python
 - PyTorch
 - NumPy
 
 ## Quantized SNNs for Spatio-Temporal Patterns
 
 ```
-python 
+python spytorch_precise.py
 ```
+
+You can specify desired setting either as command line arguments or within spytorch_precise.py.
+
+Optional arguments:
+
+| Argument               |  Description                                             | 
+|:----------------------:|:--------------------------------------------------------:|
+| --input INPUT          | Input pickle file (default: ./data/input_700_250_25.pkl) |
+| --target TARGET        | Target pattern pickle (default: ./data/smile95.pkl)      |
+| --global_wb GLOBAL_WB  | Weight bitwidth (default: 2)                             |
+| --global_ab GLOBAL_AB  | Membrane potential, synapse state bitwidth (default: 8)  |
+| --global_gb GLOBAL_GB  | Gradient bitwidth (default: 8)                           |
+| --global_eb GLOBAL_EB  | Error bitwidth (default: 8)                              |
+| --global_rb GLOBAL_RB  | Gradient RNG bitwidth (default: 16)                      |
+| --time_step TIME_STEP  | Simulation time step size (default: 0.001)               |
+| --nb_steps NB_STEPS    | Simulation steps (default: 250)                          |
+| --nb_epochs NB_EPOCHS  | Simulation steps (default: 10000)                        |
+| --tau_mem TAU_MEM      | Time constant for membrane potential (default: 0.01)     |
+| --tau_syn TAU_SYN      | Time constant for synapse (default: 0.005)               |
+| --tau_vr TAU_VR        | Time constant for Van Rossum distance (default: 0.005)   |
+| --alpha ALPHA          | Time constant for synapse (default: 0.75)                |
+| --beta BETA            | Time constant for Van Rossum distance (default: 0.875)   |
+| --nb_inputs NB_INPUTS  | Spatial input dimensions (default: 700)                  |
+| --nb_hidden NB_HIDDEN  | Spatial hidden dimensions (default: 400)                 |
+| --nb_outputs NB_OUTPUTS| Spatial output dimensions (default: 250)                 |
--- spytorch_precise.py
+++ spytorch_precise.py
@@ -1,19 +1,12 @@
-import os
-
-import math
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.gridspec import GridSpec
-import seaborn as sns
-import pickle
+import  argparse, pickle
 
 import torch
 import torch.nn as nn
 import torchvision
+import numpy as np
 
 import spytorch_util
 import quantization
-from quantization import quant_act, init_layer_weights, SSE, to_cat, clip, quant_w, quant_err, quant_grad, quant_generic, step_d
 
 dtype = torch.float
 
@@ -23,12 +16,60 @@ if torch.cuda.is_available():
 else:
     device = torch.device("cpu")
 
+# Code is based on: https://github.com/fzenke/spytorch
+
+parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument("--input", type=str, default="./data/input_700_250_25.pkl", help='Input pickle')
+parser.add_argument("--target", type=str, default="./data/smile95.pkl", help='Target pattern pickle')
+
+parser.add_argument("--global_wb", type=int, default=2, help='Weight bitwidth')
+parser.add_argument("--global_ab", type=int, default=8, help='Membrane potential, synapse state bitwidth')
+parser.add_argument("--global_gb", type=int, default=8, help='Gradient bitwidth')
+parser.add_argument("--global_eb", type=int, default=8, help='Error bitwidth')
+parser.add_argument("--global_rb", type=int, default=16, help='Gradient RNG bitwidth')
+
+parser.add_argument("--time_step", type=float, default=1e-3, help='Simulation time step size')
+parser.add_argument("--nb_steps", type=float, default=250, help='Simulation steps')
+parser.add_argument("--nb_epochs", type=float, default=10000, help='Simulation steps')
+
+parser.add_argument("--tau_mem", type=float, default=10e-3, help='Time constant for membrane potential')
+parser.add_argument("--tau_syn", type=float, default=5e-3, help='Time constant for synapse')
+parser.add_argument("--tau_vr", type=float, default=5e-3, help='Time constant for Van Rossum distance')
+parser.add_argument("--alpha", type=float, default=.75, help='Time constant for synapse')
+parser.add_argument("--beta", type=float, default=.875, help='Time constant for Van Rossum distance')
+
+parser.add_argument("--nb_inputs", type=int, default=700, help='Spatial input dimensions')
+parser.add_argument("--nb_hidden", type=int, default=400, help='Spatial hidden dimensions')
+parser.add_argument("--nb_outputs", type=int, default=250, help='Spatial output dimensions')
+
+args = parser.parse_args()
+
+
+quantization.global_wb = args.global_wb
+quantization.global_ab = args.global_ab
+quantization.global_gb = args.global_gb
+quantization.global_eb = args.global_eb
+quantization.global_rb = args.global_rb
+stop_quant_level = 33
+
+time_step = args.time_step
+nb_steps  = args.nb_steps
+tau_mem = args.tau_mem
+tau_syn = args.tau_syn
+tau_vr  = args.tau_vr
+
+alpha   = args.alpha
+beta    = args.beta
+
+nb_inputs  = args.nb_inputs
+nb_hidden  = args.nb_hidden
+nb_outputs = args.nb_outputs
+
 def conv_exp_kernel(inputs, time_step, tau, device):
     dtype = torch.float
     nb_hidden = inputs.shape[1]
     nb_steps = inputs.shape[0]
-    # hyper para -> what value do we need here?
-    #alpha = float(torch.exp(torch.tensor([-time_step/tau], device= device, dtype=dtype)))
 
     u = torch.zeros((nb_hidden), device=device, dtype=dtype)
     rec_u = []
@@ -66,7 +107,7 @@ class einsum_linear(torch.autograd.Function):
     @staticmethod
     def forward(ctx, input, weight, scale, bias=None):
         if quantization.global_wb < stop_quant_level:
-            w_quant = quant_w(weight, scale)
+            w_quant = quantization.quant_w(weight, scale)
         else:
             w_quant = weight
 
@@ -84,7 +125,7 @@ class einsum_linear(torch.autograd.Function):
         input, w_quant, bias = ctx.saved_tensors
         grad_input = grad_weight = grad_bias = None
         if quantization.global_eb < stop_quant_level:
-            quant_error = quant_err(grad_output)
+            quant_error = quantization.quant_err(grad_output)
         else:
             quant_error = grad_output
 
@@ -94,7 +135,7 @@ class einsum_linear(torch.autograd.Function):
 
         if ctx.needs_input_grad[1]:
             if quantization.global_gb < stop_quant_level:
-                grad_weight = quant_grad(torch.einsum("bc,bd->dc", (quant_error, input))).float()
+                grad_weight = quantization.quant_grad(torch.einsum("bc,bd->dc", (quant_error, input))).float()
             else:
                 grad_weight = torch.einsum("bc,bd->dc", (quant_error, input))
 
@@ -108,7 +149,7 @@ class custom_quant(torch.autograd.Function):
     @staticmethod
     def forward(ctx, input, b_level):
         if quantization.global_ab < stop_quant_level:
-            output, clip_info = quant_act(input)
+            output, clip_info = quantization.quant_act(input)
         else:
             output, clip_info = input, None
         ctx.save_for_backward(clip_info)
@@ -118,7 +159,7 @@ class custom_quant(torch.autograd.Function):
     def backward(ctx, grad_output):
         clip_info = ctx.saved_tensors
         if quantization.global_eb < stop_quant_level:
-            quant_error = quant_err(grad_output) * clip_info[0].float()
+            quant_error = quantization.quant_err(grad_output) * clip_info[0].float()
         else:
             quant_error = grad_output
         return quant_error, None
@@ -126,8 +167,8 @@ class custom_quant(torch.autograd.Function):
 
 def run_snn(inputs):
     with torch.no_grad():
-        spytorch_util.w1.data = clip(spytorch_util.w1.data, quantization.global_wb)
-        spytorch_util.w2.data = clip(spytorch_util.w2.data, quantization.global_wb)
+        spytorch_util.w1.data = quantization.clip(spytorch_util.w1.data, quantization.global_wb)
+        spytorch_util.w2.data = quantization.clip(spytorch_util.w2.data, quantization.global_wb)
 
 
     h1 = einsum_linear.apply(inputs, spytorch_util.w1, scale1)
@@ -221,66 +262,37 @@ def train(x_data, y_data, lr=1e-3, nb_epochs=10):
        
 spike_fn  = SuperSpike.apply 
 
-quantization.global_wb = 2
-quantization.global_ab = 8
-quantization.global_gb = 8
-quantization.global_eb = 8
-quantization.global_rb = 16
-stop_quant_level = 33
-
-
-time_step = 1e-3
-nb_steps  = 250
-tau_mem = 10e-3
-tau_syn = 5e-3
-tau_vr  = 5e-3
-lr_change = 2e-4
-
-
-
-#alpha   = float(np.exp(-time_step/tau_syn))
-alpha   = .75
-#beta    = float(np.exp(-time_step/tau_mem))
-beta    = .875
-
-
-nb_inputs  = 700
-nb_hidden  = 400
-nb_outputs = 250
-
 
 quantization.global_beta = quantization.step_d(quantization.global_wb)-.5
-with open("./data/input_700_250_25.pkl", 'rb') as f:
+with open(args.input, 'rb') as f:
     x_train = pickle.load(f).t().to(device)
 
-#x_train = torch.tensor(np.array(x_train.cpu(), dtype=bool)).double().to(device)
 
-
-with open("./data/nd70.pkl", 'rb') as f:
+with open(args.target, 'rb') as f:
     y_train = torch.tensor(pickle.load(f)).to(device)
 y_train = y_train.type(dtype)
-#y_train = torch.tensor(np.array(y_train, dtype=bool)).double().to(device)
+
 
 bit_string = str(quantization.global_wb) + str(quantization.global_ab) + str(quantization.global_gb) + str(quantization.global_eb)
 
+print("Start Training")
 print(bit_string)
 
 spytorch_util.w1 = torch.empty((nb_inputs, nb_hidden),  device=device, dtype=dtype, requires_grad=True)
-scale1 = init_layer_weights(spytorch_util.w1, 28*28).to(device)
+scale1 = quantization.init_layer_weights(spytorch_util.w1, 28*28).to(device)
 
 spytorch_util.w2 = torch.empty((nb_hidden, nb_outputs), device=device, dtype=dtype, requires_grad=True)
-scale2 = init_layer_weights(spytorch_util.w2, 28*28).to(device)
+scale2 = quantization.init_layer_weights(spytorch_util.w2, 28*28).to(device)
 
 
 quantization.global_lr = .1
-# lr = 2e-4
-loss_hist, output = train(x_train, y_train, lr = 1, nb_epochs = 20) #/step_d(16)*10
+loss_hist, output = train(x_train, y_train, lr = 1, nb_epochs = args.nb_epochs)
 
 bit_string = str(quantization.global_wb) + str(quantization.global_ab) + str(quantization.global_gb) + str(quantization.global_eb)
 
 results = {'bit_string': bit_string ,'loss_hist': loss_hist, 'output': output.cpu()}
 
-with open('results/snn_nd_precise_'+bit_string+'.pkl', 'wb') as f:
+with open('results/snn_smile_precise_'+bit_string+'.pkl', 'wb') as f:
     pickle.dump(results, f)
 
 
